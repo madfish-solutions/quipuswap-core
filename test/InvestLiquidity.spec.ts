bca95b917d8b471d6c749c0418b4ea76fae1326e
kstasi <nastasiakondaurova@gmail.com>
2020-10-18 18:04:06 +0300

    other failure tests

--- test/InvestLiquidity.spec.ts
+++ test/InvestLiquidity.spec.ts
@@ -3,7 +3,7 @@ import { strictEqual, ok, notStrictEqual, rejects } from "assert";
 import BigNumber from "bignumber.js";
 import { TezosOperationError } from "@taquito/taquito";
 
-describe("InvestLiquidity()", function () {
+describe.only("InvestLiquidity()", function () {
   it("should invest liquidity and distribute new shares by current provider", async function () {
     this.timeout(5000000);
     // create context with exchange
@@ -186,18 +186,82 @@ describe("InvestLiquidity()", function () {
   });
 
   it("should fail investment if min shares are too low", async function () {
-    // create context without exchanges
+    // create context with exchange
+    let context = await Context.init();
+
+    let tezAmount = 100;
+    let tokenAmount = 100000;
+    let newShares = 0;
+
+    // attempt to invest liquidity
+    await rejects(
+      context.pairs[0].investLiquidity(tokenAmount, tezAmount, newShares),
+      (err) => {
+        strictEqual(err.message, "Dex/wrong-params", "Error message mismatch");
+        return true;
+      },
+      "Investment to Dex should fail"
+    );
   });
 
   it("should fail investment if min shares are too high", async function () {
-    // create context without exchanges
+    // create context with exchange
+    let context = await Context.init();
+
+    let tezAmount = 100;
+    let tokenAmount = 100000;
+    let newShares = 11;
+
+    // attempt to invest liquidity
+    await rejects(
+      context.pairs[0].investLiquidity(tokenAmount, tezAmount, newShares),
+      (err) => {
+        strictEqual(err.message, "Dex/wrong-params", "Error message mismatch");
+        return true;
+      },
+      "Investment to Dex should fail"
+    );
   });
 
   it("should fail investment if too little tez are sent", async function () {
-    // create context without exchanges
+    // create context with exchange
+    let context = await Context.init();
+
+    let tezAmount = 1;
+    let tokenAmount = 100000;
+    let newShares = 11;
+
+    // attempt to invest liquidity
+    await rejects(
+      context.pairs[0].investLiquidity(tokenAmount, tezAmount, newShares),
+      (err) => {
+        strictEqual(err.message, "Dex/wrong-params", "Error message mismatch");
+        return true;
+      },
+      "Investment to Dex should fail"
+    );
   });
 
   it("should fail investment if too little tokens are sent", async function () {
-    // create context without exchanges
+    // create context with exchange
+    let context = await Context.init();
+
+    let tezAmount = 100;
+    let tokenAmount = 1000;
+    let newShares = 10;
+
+    // attempt to invest liquidity
+    await rejects(
+      context.pairs[0].investLiquidity(tokenAmount, tezAmount, newShares),
+      (err) => {
+        strictEqual(
+          err.message,
+          "NotEnoughAllowance",
+          "Error message mismatch"
+        );
+        return true;
+      },
+      "Investment to Dex should fail"
+    );
   });
 });
