8e844a8e71ad239f0a18085a8b278594dc4774a7
kstasi <nastasiakondaurova@gmail.com>
2021-03-08 16:50:06 +0200

    update storage

--- test/storage/Factory.ts
+++ test/storage/Factory.ts
@@ -1,7 +1,8 @@
 import { MichelsonMap } from "@taquito/michelson-encoder";
 
 export default {
-  token_list: [],
+  counter: "0",
+  token_list: MichelsonMap.fromLiteral({}),
   token_to_exchange: MichelsonMap.fromLiteral({}),
   dex_lambdas: MichelsonMap.fromLiteral({}),
   token_lambdas: MichelsonMap.fromLiteral({}),
