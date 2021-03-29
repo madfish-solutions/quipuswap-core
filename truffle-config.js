821355cb8f5ea3adae22f2e11230c1612c5e5196
kstasi <nastasiakondaurova@gmail.com>
2021-03-29 17:05:53 +0300

    updat metadata

--- truffle-config.js
+++ truffle-config.js
@@ -31,6 +31,13 @@ module.exports = {
       secretKey: alice.sk,
       type: "tezos",
     },
+    florence: {
+      host: "https://florence-tezos.giganode.io",
+      port: 443,
+      network_id: "*",
+      secretKey: alice.sk,
+      type: "tezos",
+    },
     mainnet: {
       host: "https://mainnet.smartpy.io",
       port: 443,
